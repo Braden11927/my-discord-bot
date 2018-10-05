1522752d50aa3fc0786541c25c441d5cd9cb1932
Braden11927 <40736002+Braden11927@users.noreply.github.com>
2018-10-05 16:11:29 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,29 +1,16 @@
-const Discord = require('discord.js');
 
+const Discord = require('discord.js');
 const client = new Discord.Client();
 
- 
-
 client.on('ready', () => {
-
     console.log('I am ready!');
-
 });
-
- 
 
 client.on('message', message => {
-
     if (message.content === 'ping') {
-
-       message.reply('pong');
-
-       }
-
+    	message.reply('pong');
+  	}
 });
 
- 
-
 // THIS  MUST  BE  THIS  WAY
-
-client.login(process.env.NDkyNzA4NTU4NzczOTQ0MzMw.DobSWA.ezMvAV0uEW_BazyI7dJOmI6NVoc);//where BOT_TOKEN is the token of our bot
+client.login(process.env.NDkyNzA4NTU4NzczOTQ0MzMw.DobSWA.ezMvAV0uEW_BazyI7dJOmI6NVoc);//where BOT_TOKEN is the token of our bot 
